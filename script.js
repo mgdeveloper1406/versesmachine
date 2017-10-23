67dd1b66f507439080b8bdc2cd47bbc14b3c4902
Claude Lionel NKAYA MABONDZOT <lionel.nkaya@gmail.com>
2017-10-23 05:13:28 +0000

    Adding jQuery

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-$ (document).ready(function (){
-    $('p').hide();
-});
+    var quoteList = ["'The Lord will fight for you, and you shall hold your peace.'", "'He gives power to the weak,And to those who have no might He increases strength.' "];
+
+     var authorList = [" - Exodus 14:14", " - Isaiah 40:29" ];
+     
+     var quote = document.getElementsByClassName("quote");
+     
+     var author = document.getElementsByClassName("author");
+     
+     var test = "Hello World";
+
+  
+  $( document ).ready(function() {
+     
+     quote.textContent = test;
+     
+     alert("Making sure jquerry works");
+ });
