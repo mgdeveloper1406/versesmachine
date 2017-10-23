84b918077a847a8f4ceb7c37c5e45b770cb125e9
Claude Lionel NKAYA MABONDZOT <lionel.nkaya@gmail.com>
2017-10-23 06:08:48 +0000

    Jquerry works

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
     var quoteList = ["'The Lord will fight for you, and you shall hold your peace.'", "'He gives power to the weak,And to those who have no might He increases strength.' "];
 
-     var authorList = [" - Exodus 14:14", " - Isaiah 40:29" ];
+     var authorList = ["Exodus 14:14", "Isaiah 40:29" ];
      
-     var quote = document.getElementsByClassName("quote");
+     var quote = document.getElementById("quote");
      
-     var author = document.getElementsByClassName("author");
-     
-     var test = "Hello World";
+     var author = document.getElementById("author");
 
   
   $( document ).ready(function() {
      
-     quote.textContent = test;
+     quote.textContent = quoteList[0];
+     author.textContent = authorList[0];
      
-     alert("Making sure jquerry works");
+     alert("Making sure jquerry still works");
  });
