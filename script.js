5039c013950be8fc8ed78d06a0819c79d3328c70
Claude Lionel NKAYA MABONDZOT <lionel.nkaya@gmail.com>
2017-10-29 19:17:26 +0000

    on click function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-    var quoteList = ["'The Lord will fight for you, and you shall hold your peace.'", "'He gives power to the weak,And to those who have no might He increases strength.' "];
+    var quoteList = ["'The Lord will fight for you, and you shall hold your peace.'", "'He gives power to the weak,And to those who have no might He increases strength.'", "'Therefore if the Son makes you free, you shall be free indeed.'" ];
 
-     var authorList = ["Exodus 14:14", "Isaiah 40:29" ];
+     var authorList = ["Exodus 14:14", "Isaiah 40:29", "John 8:36" ];
      
      var quote = document.getElementById("quote");
      
@@ -14,8 +14,8 @@
      button.onclick = function () {
          
         for (var i = 0; i < quoteList.length; i++) {
-           quote.textContent = quoteList[0];
-           author.textContent = authorList[0];
+           quote.textContent = quoteList[i];
+           author.textContent = authorList[i];
         } 
 
      }
