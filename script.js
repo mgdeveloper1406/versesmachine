a8532974d3e6982d810d48bb89031b1b93b51ebd
Claude Lionel NKAYA MABONDZOT <lionel.nkaya@gmail.com>
2018-02-12 03:08:08 +0000

    Adding quotes + changing entry verse

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
         "'The Lord will fight for you, and you shall hold your peace.'", 
         "'He gives power to the weak, and to those who have no might He increases strength.'", 
         "'Therefore if the Son makes you free, you shall be free indeed.'",
-        "'You, Lord, keep my lamp burning; my God turns my darkness into light.'"
+        "'You, Lord, keep my lamp burning; my God turns my darkness into light.'",
+        "'For I am the LORD your God who takes hold of your right hand and says to you : do not fear; I will help you.' ",
+        "'If any of you lacks wisdom, let him ask God, who gives generously to all without reproach, and it will be given him.'",
         ];
 
      var authorList = [
      "Exodus 14:14",
      "Isaiah 40:29",
      "John 8:36",
-     "Psalm 18:28"
+     "Psalm 18:28",
+     "Isaiah 41:13",
+     "James 1:5",
      ];
      
      var quote = document.getElementById("quote");
@@ -23,9 +27,9 @@
       
 
      button.onclick = function () {
-           var randomNumberBetween0and2 = Math.floor((Math.random() * 3) + 0);
-           quote.textContent = quoteList[randomNumberBetween0and2];
-           author.textContent = authorList[randomNumberBetween0and2];
+           var randomNumberBetween0and5 = Math.floor((Math.random() * 5) + 0);
+           quote.textContent = quoteList[randomNumberBetween0and5];
+           author.textContent = authorList[randomNumberBetween0and5];
 
 
      }
