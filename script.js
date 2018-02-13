e025d05b2f030b9888a911332aa537c12d7253ca
Claude Lionel NKAYA MABONDZOT <lionel.nkaya@gmail.com>
2018-02-13 03:00:39 +0000

    Adding tweet functionality

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
         "'The Lord will fight for you, and you shall hold your peace.'", 
         "'He gives power to the weak, and to those who have no might He increases strength.'", 
         "'Therefore if the Son makes you free, you shall be free indeed.'",
-        "'You, Lord, keep my lamp burning; my God turns my darkness into light.'",
-        "'For I am the LORD your God who takes hold of your right hand and says to you : do not fear; I will help you.' ",
+        "'You, Lord, keep my lamp burning, my God turns my darkness into light.'",
+        "'For I am the LORD your God who takes hold of your right hand and says to you : do not fear, I will help you.' ",
         "'If any of you lacks wisdom, let him ask God, who gives generously to all without reproach, and it will be given him.'",
         ];
 
@@ -16,19 +16,17 @@
      "James 1:5",
      ];
      
-     var quote = document.getElementById("quote");
+     var quote = document.getElementById("quote"); 
      
      var author = document.getElementById("author");
      
      var button = document.getElementById("switch");
-     
-     var tweetBtn = document.getElementById("tweet_btn");
-     
+    
      var randomNumberBetween0and5;
      
-     var tweetQuote;
      
-     var tweetAuthor;
+     
+     
      
 
   
@@ -38,18 +36,26 @@
      button.onclick = function () {
          
            randomNumberBetween0and5 = Math.floor(Math.random() * quoteList.length);
-           quote.textContent = quoteList[randomNumberBetween0and5];
-           author.textContent = authorList[randomNumberBetween0and5];
-           tweetQuote = quoteList.split(quoteList[randomNumberBetween0and5]);
-           tweetAuthor = authorList.split(authorList[randomNumberBetween0and5]);
-           
-
-     }
-    
-  $(".twitter-share-button").click(function() {
-    $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + tweetQuote + tweetAuthor );
+           quote.innerHTML = quoteList[randomNumberBetween0and5] ;
+           author.innerHTML = authorList[randomNumberBetween0and5];
+           var tweet = quote.textContent + " " + " - " + author.textContent;
+        
+        
+          $(".twitter-share-button").click(function() {
+    $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + tweet );
     
   });
+     }
+    
+    //This is just in case the user wants to tweet the initial quote
+    var tweetInit = quote.textContent + " " + " - " + author.textContent;
+              $(".twitter-share-button").click(function() {
+    $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + tweetInit );
+    
+  });
+    
+    
+
   
  });
  
